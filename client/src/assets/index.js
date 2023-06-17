3827085d2a144c064e2f420cc38e734eea0ab5c4
Atom <anuranjan.vikas31@gmail.com>
2023-06-18 01:23:33 +0530

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import logo4 from './logo4.svg';
+
+export {
+  download,
+  logo,
+  preview,
+  logo4,
+};
