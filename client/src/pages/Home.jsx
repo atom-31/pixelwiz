538ff37b15cf5030949fff7310757aeef1549a53
Atom <anuranjan.vikas31@gmail.com>
2023-06-19 20:20:46 +0530

    fifth commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -111,6 +111,9 @@ const Home = () => {
                     </>
                 )}
             </div>
+            <div className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>
+                Done by Anuranjan Vikas
+            </div>
         </section>
     )
 }
